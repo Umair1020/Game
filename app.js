1e5e62b96ab7bfb69572b2f1edc281dca27646f3
Umair1020 <81970649+Umair1020@users.noreply.github.com>
2021-09-19 18:40:22 +0500

    Update app.js

--- app.js
+++ app.js
@@ -3,16 +3,19 @@ firebase.initializeApp(firebaseConfig);
 function login() {
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
+    console.log(email, password);
 
 
     firebase.auth().signInWithEmailAndPassword(email, password)
         .then((userCredential) => {
             // Signed in
             const user = userCredential.user;
+            // ...
         })
         .catch((error) => {
             const errorCode = error.code;
             const errorMessage = error.message;
+            console.log(errorMessage);
             // ..
         });
 
@@ -66,6 +69,7 @@ auth.onAuthStateChanged((user) => {
             lastLoggedInAt: new Date()
         });
         setData(user);
+        // setMessages();
         document.getElementById("user").innerHTML = user.email;
         document.getElementById("login_box").style.display = "none";
         document.getElementById("welcome_box").style.display = "block";
@@ -86,6 +90,61 @@ const setData = (user) => {
     });
 }
 
+let popped = 0;
+let lives = 3
+document.addEventListener('mouseover', function(e){
+    if(e.className == "blue-box"){
+        lives--
+        alert("you lost your first life")
+    }
+})
+document.addEventListener('mouseover', function(e){
+    
+    if (e.target.className === "red-box"){
+                e.target.style.backgroundColor = "#ededed";
+                popped++;
+                removeEvent(e);
+                checkAllPopped();
+    }   
+});
+document.addEventListener('mouseover' , function (e){
+
+    if(e.target.className == "blue-box"){
+     alert("you Lost");
+     window.location = 'index.html'
+     lives--;
+    }else if(e.target.className == "green-box"){
+        alert("you Lost");
+        window.location = 'index.html'
+        lives--;
+    }
+})
+function removeEvent(e){
+    e.target.removeEventListener('mouseover', function(){
+        
+    })
+};
+
+function checkAllPopped(){
+    if (popped === 24){
+        console.log('all popped!');
+        let gallery = document.querySelector('#yay-no-balloons');
+        let message = document.querySelector('#box');
+        gallery.innerHTML = '';
+        message.style.display = 'block';
+        if(e.target.className){
+            alert("Level 1 is complete")
+          }
+    }
+};
+
+const next = () =>{
+        window.location.href = "index2.html"
+}
+
+const Reset = () => {
+    window.location.href = "index.html"
+}
 let popped = 0;
 
 document.addEventListener('mouseover', function(e){
